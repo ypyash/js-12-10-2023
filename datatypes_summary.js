92482fae6e84f1f74d5ec5f9d3a0fc1a8c9c13cd
ypyash30 <114346498+ypyash30@users.noreply.github.com>
2023-10-15 11:56:57 +0000

    datatypes summary

---
+++ datatypes_summary.js
@@ -0,0 +1,27 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.33
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Function
+
+const heros = ["shaktiman", "nagraj", "doga"]
+let myObj = {
+    name : "Yash",
+    age : 22
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
