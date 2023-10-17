3ecc860ffbb6e9664b7f25559e3f6b1843e3ffa5
ypyash30 <114346498+ypyash30@users.noreply.github.com>
2023-10-17 08:12:56 +0000

    math and num

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,38 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.6266
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 198987
+
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,2,3,5,6));
+// console.log(Math.max(4,2,3,5,6));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
