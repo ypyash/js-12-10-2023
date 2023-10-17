14a537475841fb10d65e515e5aea83ac439e4ba3
ypyash30 <114346498+ypyash30@users.noreply.github.com>
2023-10-17 04:32:33 +0000

    datatypes

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -25,3 +25,28 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello World");
 }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "Yash"
+
+let anotherName = myName
+anotherName = "Ronak"
+
+console.log(anotherName);
+console.log(myName);
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+console.log(user1)
+let user2 = user1
+
+user2.email = "yash@gmail.com"
+
+console.log(user2);
+console.log(user1);
