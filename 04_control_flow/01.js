7944df3e641bb0caa72d52f9445ded255262cfdb
ypyash <51624490+ypyash@users.noreply.github.com>
2023-10-19 11:26:21 +0000

    control flow

---
+++ 04_control_flow/01.js
@@ -0,0 +1,45 @@
+// if
+// const isUserLoggedIn = true
+// const temperature = 41
+
+
+// if (temperature < 50){
+//     console.log("executed");
+// }
+// else{
+//     console.log("not executed");
+// }
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// const balance = 1000
+
+// //if (balance > 500) console.log("test");
+
+// if (balance < 500){
+//     console.log("less than ");
+// }
+// else if (balance < 750){
+//     console.log("less than 750");
+// }
+// else if (balance < 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+
+const isUserLoggedIn = true
+const debitCard = true
+
+if (isUserLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+}
