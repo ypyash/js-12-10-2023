e0abda1b1f6e988ab6d9d4a1883fe40ec5060a20
ypyash <51624490+ypyash@users.noreply.github.com>
2023-10-20 05:33:29 +0000

    control flow

--- 04_control_flow/01.js
+++ 04_control_flow/01.js
@@ -39,7 +39,13 @@
 
 const isUserLoggedIn = true
 const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
 
 if (isUserLoggedIn && debitCard) {
     console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
 }
