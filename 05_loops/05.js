38f33dffd901e050f618dd0be5f6e95f596d1723
ypyash <51624490+ypyash@users.noreply.github.com>
2023-10-20 11:17:20 +0000

    loops

---
+++ 05_loops/05.js
@@ -0,0 +1,39 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+})
+
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFile: "js"
+    },
+    {
+        languageName: "Java",
+        languageFile: "java"
+    },
+    {
+        languageName: "Python",
+        languageFile: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})
