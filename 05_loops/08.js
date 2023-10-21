ec2224c138d48e82be42ee94306e52933330a733
ypyash <51624490+ypyash@users.noreply.github.com>
2023-10-21 08:14:40 +0000

    filter map & reduce

---
+++ 05_loops/08.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, curval) {
+//     console.log(`acc : ${acc} and curval : ${curval}`);
+//     return acc + curval
+// }, 0)
+
+// const myTotal = myNums.reduce( (acc, curval) => acc + curval ,0)
+
+// console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+
+const myTotal = shoppingCart.reduce( (acc, item) => acc + item.price ,0)
+
+console.log(myTotal);
